5615820489ef44e372c053fb54219e545395a0ae
John Ludlow <john.ludlow@halfords.co.uk>
2024-06-07 20:34:59 +0100

    Add DodgeTheCreeps

---
+++ DodgeTheCreeps/Main.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	private int _score;
+
+	[Export]
+	public PackedScene MobScene {get;set;}
+
+	private void OnPlayerHit()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+
+		GetNode<HeadsUpDisplay>("HeadsUpDisplay").ShowGameOver();
+		GetNode<AudioStreamPlayer2D>("Music").Stop();
+		GetNode<AudioStreamPlayer2D>("DeathSound").Play();
+	}
+
+	private void OnMobTimerTimeout()
+	{
+		var mob = MobScene.Instantiate<Mob>();
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+		
+		mob.Position = mobSpawnLocation.Position;
+		
+		var direction = (float)(mobSpawnLocation.Rotation + Math.PI / 2);
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Math.PI / 4);
+		mob.Rotation = direction;
+
+        mob.LinearVelocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+
+		AddChild(mob);
+	}
+
+	private void OnScoreTimerTimeout()
+	{
+		GetNode<HeadsUpDisplay>("HeadsUpDisplay").UpdateScore(++_score);
+	}
+
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+
+	protected void NewGame()
+	{
+		_score = 0;
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+
+		var hud = GetNode<HeadsUpDisplay>("HeadsUpDisplay");
+		hud.UpdateScore(_score);
+		hud.ShowMessage("GET READY!");
+
+		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
+
+		GetNode<AudioStreamPlayer2D>("Music").Play();
+	}
+}
---
+++ DodgeTheCreeps/Mob.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public partial class Mob : RigidBody2D
+{
+	private void OnVisbleOnScreenNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+
+    public override void _Ready()
+    {
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		var mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+    }	
+}
